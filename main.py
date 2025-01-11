531c6570d1531a463aa38e756c98fa6b7398ea00
Hugo <hugo.r.roussel@gmail.com>
2025-01-11 16:20:36 +0900

    First assignment

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import constants as const
+
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {const.SCREEN_WIDTH}")
+    print(f"Screen height: {const.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
